30377885e6807cb65781291c69078fbc984494ba
ehsanvs65 <vahidiehsans6565@gmail.com>
2024-08-11 14:17:05 +0330

    second commit

---
+++ Resume.Web/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Bussines.Services.Interface;
+using Resume.DAL.ViewModels.User;
+
+namespace Resume.Web.Areas.Admin.Controllers;
+
+public class UserController:Controller
+{
+    #region Fields
+
+    private readonly IUserService _userService;
+
+    #endregion
+
+    #region Constructor
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    #endregion
+
+    #region Actions
+
+    #region List
+
+    public async Task<IActionResult> List()
+    {
+        return View();
+    }
+
+    #endregion
+
+    #region Create
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateUserViewModel model)
+    {
+        #region Validations
+
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        #endregion
+        return View();
+    }
+
+    #endregion
+
+    #region Update
+
+    public async Task<IActionResult> Update(int id)
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Update(EditUserViewModel model)
+    {
+        #region Validations
+
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        #endregion
+        return View();
+    }
+
+    #endregion
+
+    #endregion
+}
